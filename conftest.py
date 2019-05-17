03cda09eb9d2df16c04ca152597639d56e435a6a
Dmitriy Yakovlev <dmitriy.m.yakovlev@gmail.com>
2019-05-17 11:02:50 +0700

    Подготовка нового проекта Mantis

---
+++ conftest.py
@@ -0,0 +1,46 @@
+from fixture.application import Application
+import pytest
+import json
+import os.path
+
+
+
+fixture = None
+config_file = None
+
+
+def load_config(file):
+    global config_file
+    if config_file is None:
+        config_file_json = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file_json) as f:
+            config_file = json.load(f)
+    return config_file
+
+
+@pytest.fixture
+def app(request):
+    global fixture
+
+    browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption("--target"))['web']
+
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=web_config["baseUrl"])
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.destroy()
+
+    request.addfinalizer(fin)
+    return fixture
+
+
+# parametri komandnoi ctroki
+# hook function https://docs.pytest.org/en/latest/parametrize.html#basic-pytest-generate-tests-example
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--target", action="store", default="target.json")
